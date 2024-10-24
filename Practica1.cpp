d0e170af7308c6aa1f37b95f3a18950dacdf2141
LuisChow <Luisfchow69@gmail.com>
2024-10-24 00:41:51 -0400

    Segundo commit

--- Practica1.cpp
+++ Practica1.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 
 int main(){
+    int dato;
+    string nombre;
+    int cedula;
+    string correo;
 
     ofstream output("./data/data.txt");
+
+    cout<<"Cuantos usuarios va a registrar? (Numeros enteros)"<<endl;
+    cin>>dato;
+
+    for(int i=0; i<dato; i++){
+        cout<<"Introduzca el nombre"<<endl;
+        cin>>nombre;
+        cout<<"Introduzca la cedula"<<endl;
+        cin>>cedula;
+        cout<<"Introduzca el correo"<<endl;
+        cin>>correo;
+        output<<nombre<<" ";
+        output<<cedula<<" ";
+        output<<correo<<endl;
+    }
+    output.close();
     return 0;
 }
